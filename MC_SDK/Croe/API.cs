4eff2244d4f962109c6ee55d2aa4b429dffabf43
wangpeiyang <wpyok500@gmail.com>
2021-09-30 17:26:16 +0800

    update

--- MC_SDK/Croe/API.cs
+++ MC_SDK/Croe/API.cs
@@ -37,6 +37,12 @@ namespace MC_SDK.Croe
         delegate IntPtr SendGroupMsg(long GroupQQ, [MarshalAs(UnmanagedType.LPStr)] string msg,long otherQQ);
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
         delegate IntPtr UploadImage(long QQ, [MarshalAs(UnmanagedType.LPArray)] byte[] pic, int picsize);
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        delegate int SendGroupTempMsg(long QQ, long otherQQ, [MarshalAs(UnmanagedType.LPStr)] string msg);
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        delegate IntPtr SendConsultMsg(long QQ, [MarshalAs(UnmanagedType.LPStr)] string msg, [MarshalAs(UnmanagedType.LPStr)] string chattoken);
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        delegate IntPtr AddGroup([MarshalAs(UnmanagedType.LPStr)] string GroupQQ, [MarshalAs(UnmanagedType.LPStr)] string answer);
 
         /// <summary>
         /// 输出日志
@@ -81,6 +87,93 @@ namespace MC_SDK.Croe
             int MsgAddress = int.Parse(JObject.Parse(apidata).SelectToken("发送群消息").ToString());
             SendGroupMsg sendmsg = (SendGroupMsg)Marshal.GetDelegateForFunctionPointer(new IntPtr(MsgAddress), typeof(SendGroupMsg));
             string ret = Marshal.PtrToStringAnsi(sendmsg(QQ, msg, otherQQ));
+            sendmsg = null;
+            return ret;
+        }
+        /// <summary>
+        /// 发送好友消息_纯文本
+        /// </summary>
+        /// <param name="QQ"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public string SendPrivateMsg_C(long QQ, string msg)
+        {
+            int MsgAddress = int.Parse(JObject.Parse(apidata).SelectToken("发送好友消息_纯文本").ToString());
+            SendPrivateMsg sendmsg = (SendPrivateMsg)Marshal.GetDelegateForFunctionPointer(new IntPtr(MsgAddress), typeof(SendPrivateMsg));
+            string ret = Marshal.PtrToStringAnsi(sendmsg(QQ, msg));
+            sendmsg = null;
+            return ret;
+        }
+        /// <summary>
+        /// 发送群消息_纯文本
+        /// </summary>
+        /// <param name="QQ"></param>
+        /// <param name="msg"></param>
+        /// <param name="otherQQ"></param>
+        /// <returns></returns>
+        public string SendGroupMsg_C(long QQ, string msg, long otherQQ)
+        {
+            int MsgAddress = int.Parse(JObject.Parse(apidata).SelectToken("发送群消息_纯文本").ToString());
+            SendGroupMsg sendmsg = (SendGroupMsg)Marshal.GetDelegateForFunctionPointer(new IntPtr(MsgAddress), typeof(SendGroupMsg));
+            string ret = Marshal.PtrToStringAnsi(sendmsg(QQ, msg, otherQQ));
+            sendmsg = null;
+            return ret;
+        }
+        /// <summary>
+        /// 发送群临时消息
+        /// </summary>
+        /// <param name="QQ"></param>
+        /// <param name="msg"></param>
+        /// <param name="otherQQ"></param>
+        /// <returns></returns>
+        public int SendGroupTempMsg_(long QQ, long otherQQ, string msg)
+        {
+            int MsgAddress = int.Parse(JObject.Parse(apidata).SelectToken("发送群临时消息").ToString());
+            SendGroupTempMsg sendmsg = (SendGroupTempMsg)Marshal.GetDelegateForFunctionPointer(new IntPtr(MsgAddress), typeof(SendGroupTempMsg));
+            int ret = sendmsg(QQ, otherQQ, msg);
+            sendmsg = null;
+            return ret;
+        }
+        /// <summary>
+        /// 发送咨询消息
+        /// </summary>
+        /// <param name="QQ"></param>
+        /// <param name="msg"></param>
+        /// <param name="chattoken">可以通过chattoken回复别人的咨询消息,就是对方可以不用开通咨询</param>
+        /// <returns></returns>
+        public string SendConsultMsg_(long QQ, string msg, string chattoken = "")
+        {
+            int MsgAddress = int.Parse(JObject.Parse(apidata).SelectToken("发送咨询消息").ToString());
+            SendConsultMsg sendmsg = (SendConsultMsg)Marshal.GetDelegateForFunctionPointer(new IntPtr(MsgAddress), typeof(SendConsultMsg));
+            string ret = Marshal.PtrToStringAnsi(sendmsg(QQ, msg, chattoken));
+            sendmsg = null;
+            return ret;
+        }
+        /// <summary>
+        /// 添加好友
+        /// </summary>
+        /// <param name="QQ"></param>
+        /// <param name="answer">问题答案</param>
+        /// <returns></returns>
+        public string AddFriend_(long QQ, string answer = "")
+        {
+            int MsgAddress = int.Parse(JObject.Parse(apidata).SelectToken("添加好友").ToString());
+            SendPrivateMsg sendmsg = (SendPrivateMsg)Marshal.GetDelegateForFunctionPointer(new IntPtr(MsgAddress), typeof(SendPrivateMsg));
+            string ret = Marshal.PtrToStringAnsi(sendmsg(QQ, answer));
+            sendmsg = null;
+            return ret;
+        }
+        /// <summary>
+        /// 添加群
+        /// </summary>
+        /// <param name="GroupQQ"></param>
+        /// <param name="answer">验证消息</param>
+        /// <returns></returns>
+        public string AddGroup_(long GroupQQ, string answer = "")
+        {
+            int MsgAddress = int.Parse(JObject.Parse(apidata).SelectToken("添加群").ToString());
+            AddGroup sendmsg = (AddGroup)Marshal.GetDelegateForFunctionPointer(new IntPtr(MsgAddress), typeof(AddGroup));
+            string ret = Marshal.PtrToStringAnsi(sendmsg(GroupQQ.ToString(), answer));
             sendmsg = null;
             return ret;
         }
